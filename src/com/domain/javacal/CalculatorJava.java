d637ddf0aadf824b0693049c9ca62d43eee7f3a9
Suleman <Suleman@gmail.com>
2019-04-01 21:42:30 -0700

    first commit

---
+++ src/com/domain/javacal/CalculatorJava.java
@@ -0,0 +1,20 @@
+package com.domain.javacal;
+
+public class CalculatorJava {
+
+
+	public static int sum(int a, int b) {
+
+		return a + b;
+
+	}
+
+	public static int subtract(int a, int b) {
+
+		return a - b;
+	}
+
+	
+	
+	
+}
---
+++ src/com/domain/javacal/Main.java
@@ -0,0 +1,13 @@
+package com.domain.javacal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CalculatorJava cal= new CalculatorJava();
+		int sumNum=cal.sum(10, 10);
+		System.out.println(sumNum);
+		
+		System.out.println(cal.subtract(10, 8));
+	}
+
+}
